094663c63dd4747c73c37c587374bff8b10828d1
Ruben Rådelius <ruben.radelius@gmail.com>
2021-02-16 12:30:40 +0100

    adds GC collect to LoadUnload script

--- Assets/Scripts/ResourcesLoadUnload.cs
+++ Assets/Scripts/ResourcesLoadUnload.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class ResourcesLoadUnload : MonoBehaviour {
     private int _index;
@@ -11,6 +13,7 @@ public class ResourcesLoadUnload : MonoBehaviour {
     public void NextSkybox() {
         Destroy(_currentCamera);
         Resources.UnloadUnusedAssets();
+        GC.Collect();
         
         if (_index < 5) 
             _index++;
