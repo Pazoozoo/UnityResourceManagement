3da90f13ca8e6c3c1bcb041e97941aa4d9b2c399
Ruben Rådelius <ruben.radelius@gmail.com>
2021-02-16 11:24:48 +0100

    adds AllInOneScene solution

---
+++ Assets/Scripts/ToggleSkyboxGameObjects.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ToggleSkyboxGameObjects : MonoBehaviour {
+    public GameObject[] cameras;
+    private int _index;
+
+    private void Start() {
+        foreach (var c in cameras) {
+            c.SetActive(false);
+        }
+        cameras[_index].SetActive(true);
+    }
+
+    public void NextSkybox() {
+        cameras[_index].SetActive(false);
+        if (_index < cameras.Length -1) 
+            _index++;
+        else 
+            _index = 0;
+        
+        cameras[_index].SetActive(true);
+    }
+}
